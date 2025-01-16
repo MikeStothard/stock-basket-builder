1704ac9bd25762fcaa3379958276578492a7e7cc
Michael Stothard <michaelstothard@Michaels-MacBook-Pro-5.local>
2025-01-16 17:15:35 +0000

    Third commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google';
 import '@mantine/core/styles.css';
 import React from 'react';
 import { Providers } from './providers';
-import { Navigation } from '@/components/Navigation';
+import { Navigation } from '../components/Navigation';
 
 const inter = Inter({ subsets: ['latin'] });
 
